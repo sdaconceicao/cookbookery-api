ff34d282effdbb26512b6ceb4265466f66830f09
Stephen da Conceicao <daconcei472@gmail.com>
2019-03-06 20:59:19 -0500

    Added swagger openapi documentation

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import cors from 'cors';
 import minimist from 'minimist';
 import logger from 'morgan';
 
+
 import {setupRoutes} from './routes';
 
 const app = express(),
     args = process.argv.slice(2),
     port = minimist(args).port || 3000;
+
+
 
 app.use(logger('dev'));
 app.use(cors());
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
+import swaggerUi from 'swagger-ui-express';
+
 import RecipesRouter from './Recipes.route';
 import TagsRouter from './Tags.route';
 
 export const setupRoutes = app => {
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(require("../config/swagger")));
     app.use('/recipes', RecipesRouter);
     app.use('/tags', TagsRouter);
 };
